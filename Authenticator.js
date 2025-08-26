ca6583eda07b91b4512311e42e68a29ca4a5b8b6
MatheusVolponi <matheusponath20@gmail.com>
2025-08-26 14:30:19 -0300

    feat: aprimoramento do autenticador usando duck type.

--- Authenticator.js
+++ Authenticator.js
@@ -1,6 +1,13 @@
 class Authenticator {
     static login(authenticable, password) {
+        if(Authenticator.isAutenticatable(authenticable)) {
         return authenticable.authenticate(password);
+        }
+        return false;
+    }
+
+    static isAutenticatable(authenticable) {
+        return "authenticate" in authenticable && authenticable.authenticate instanceof Function;
     }
 }
 
